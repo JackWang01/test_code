cd633d2814c72d72aa7f05feb35df5d870924452
jackwang <1961981721@qq.com>
2024-05-05 22:52:06 +0800

    test.c code

---
+++ test.c
@@ -0,0 +1,13 @@
+//生成一段c语言的代码
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    //scanf("%d %d", &num1, &num2);
+    int num1 = 10;
+    int num2 = 20;
+    
+    sum = num1 + num2;
+    printf("两个整数的和为：%d\n", sum);
+}
