e8267a587ce88ff047f4e663ea559de38bbcd577
jackwang <1961981721@qq.com>
2024-05-05 22:57:57 +0800

    modify test.c

--- test.c
+++ test.c
@@ -5,8 +5,8 @@ int main() {
     int num1, num2, sum;
 
     //scanf("%d %d", &num1, &num2);
-    int num1 = 10;
-    int num2 = 20;
+    num1 = 10;
+    num2 = 20;
     
     sum = num1 + num2;
     printf("两个整数的和为：%d\n", sum);
